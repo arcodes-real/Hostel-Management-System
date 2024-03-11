1acb56fa7785163c50ad0a0c2af5d11a144ef72f
Archisman Datta <mpd19i017@iiitdm.ac.in>
2024-03-11 23:06:58 +0530

    Pages and components created

---
+++ src/Components/Topbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Topbar() {
+  return (
+    <div>
+      <h2>Hostel Management System</h2>
+    </div>
+  )
+}
---
+++ src/Pages/ClientLanding.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Topbar from '../Components/Topbar'
+
+export default function ClientLanding() {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  )
+}
